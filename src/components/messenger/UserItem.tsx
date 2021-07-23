9610b8c8e74e16bc0c39172a4ace8fbf0bb65289
Bordin Intarama <bordin@odds.team>
2021-07-23 11:43:53 +0700

    Modified user chat item

--- src/components/messenger/UserItem.tsx
+++ src/components/messenger/UserItem.tsx
@@ -4,7 +4,7 @@ import moreButton from "../../assets/img/more_horiz_white_24dp.svg";
 import videoCallButton from "../../assets/img/video_call_white_24dp.svg";
 
 const UserItem: React.FC = () => {
-  const numberOfUser = Array.of(1);
+  const numberOfUser = Array.of(1, 2, 3, 4, 5, 6, 7);
   const [searchMessage, setSearchMessage] = useState<string | undefined>("");
 
   return (
@@ -25,7 +25,7 @@ const UserItem: React.FC = () => {
             <img src={videoCallButton} />
           </div>
           <div
-            className="new-message-button icon-background cursor-pointer"
+            className="new-message-button icon-background cursor-pointer ml-2"
             id="new_message"
           >
             <img src={newMessageButton} />
@@ -33,7 +33,7 @@ const UserItem: React.FC = () => {
         </div>
       </div>
 
-      <div className="search-box">
+      <div className="search-box mb-4">
         <input
           className="search-input w-full"
           placeholder="Search Messenger"
@@ -43,9 +43,30 @@ const UserItem: React.FC = () => {
           }
         />
       </div>
-      {/* {numberOfUser.map((_, index) => {
-        return <img key={index} src={"https://source.unsplash.com/random"} />;
-      })} */}
+
+      <div className="user-list-box">
+        {numberOfUser.map((_, index) => {
+          return (
+            <div key={index} className="user-list">
+              <img
+                className="user-image mr-3"
+                alt="."
+                key={index}
+                src={`https://source.unsplash.com/random`}
+              />
+              <div className="user-content">
+                <label>Bordin Intarama</label>
+                <div className="flex">
+                  <p>You: 5555</p>
+                  <p className="ml-1 mr-1"> · </p>
+                  <p>30 นาที</p>
+                </div>
+              </div>
+              <div className="text-right flex-auto">Hi</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
